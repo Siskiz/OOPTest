90a2c07c8bb7e7b30cf346342c09e82f3383b47f
Siskiz <markevi4.Alex@yandex.ru>
2022-03-25 01:07:42 +0300

    Create new interface

---
+++ src/OpenCloseBD.java
@@ -0,0 +1,17 @@
+public interface OpenCloseBD {
+
+    /*
+    This I will want to create function. It starts work, with add in buffer all information, from bd
+     */
+//    public List<Employee> startWork (List<Employee> bd, List<Employee> buffer) {
+//
+//    }
+
+    /*
+    This I would like to end work and alter information in bd new information from buffer
+     */
+//    public List<Employee> endWork (List<Employee> bd, List<Employee> buffer) {
+//
+//    }
+
+}
